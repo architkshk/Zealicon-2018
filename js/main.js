0596108302a86e4a67ce7fe73e8fab81e326e1c0
Archit Kaushik <asarchit65@gmail.com>
2018-02-09 14:49:18 +0530

    new homepage

--- js/main.js
+++ js/main.js
@@ -3,3 +3,15 @@ $(".view, .island, .grass1, .grass2").mouseover(function(){
 	$('.grass1').addClass("grass1Hover");
 	$('.grass2').addClass("grass2Hover");
 });
+$(document).keydown(function(e){
+	if(e.keyCode==38){
+		$('.island').addClass("islandHover");
+		$('.grass1').addClass("grass1Hover");
+		$('.grass2').addClass("grass2Hover");
+	}
+	else if(e.keyCode==40){
+		$('.island').removeClass("islandHover");
+		$('.grass1').removeClass("grass1Hover");
+		$('.grass2').removeClass("grass2Hover");
+	}
+});
