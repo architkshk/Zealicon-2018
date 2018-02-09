b28fab174f64367c22e9ea94a57bfdf70b73717f
Archit Kaushik <asarchit65@gmail.com>
2018-02-09 14:13:04 +0530

    transition

---
+++ js/main.js
@@ -0,0 +1,5 @@
+$(".view, .island, .grass1, .grass2").mouseover(function(){
+	$('.island').addClass("islandHover");
+	$('.grass1').addClass("grass1Hover");
+	$('.grass2').addClass("grass2Hover");
+});
