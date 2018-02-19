b09531117e6ab25d7a0a23d7e96f237e79730b2f
Archit Kaushik <asarchit65@gmail.com>
2018-02-19 15:16:30 +0530

    archit #5

--- js/main.js
+++ js/main.js
@@ -9,11 +9,11 @@ $(document).ready(function(){
 		$('.antiscroll-wrap').lionbars({
 			autohide : false
 		});
-		var requestFullScreen = document.documentElement.requestFullscreen 
-		|| document.documentElement.msRequestFullscreen || document.documentElement.mozRequestFullScreen || document.documentElement.webkitRequestFullscreen;
-
-		requestFullScreen.call(document.documentElement);
     	$(window).on('load',function() {
+    		    setTimeout(function(){
+        // This hides the address bar:
+        window.scrollTo(0, 1);
+    }, 0);
     		setTimeout(function(){
 	            $('#whiteScreen').fadeIn();
     			$(".vidloader").fadeOut();
